8189de2108f8d6e434408137cfe14fe6ad2a6254
jodisaputra <jodi.saragih.29@gmail.com>
2019-12-19 15:14:42 +0700

    Latihan Laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/main')
+
+
+@section('title', 'Latihan')
+
+@section('container')
+
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-2">Hello, world!</h1>
+        </div>
+    </div>
+</div>
+
+@endsection
